67faaa1f4b4f03129ac29276c83a114f556af172
LXJ <lxj@bogon.DHCP>
2019-04-22 08:30:52 -0400

    多个bean

--- src/com/lxj/controller/HelloController.java
+++ src/com/lxj/controller/HelloController.java
@@ -7,17 +7,16 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 /**
- * IndexController
+ * HelloController
  */
-public class IndexController implements Controller {
+public class HelloController implements Controller {
 
     @Override
     public ModelAndView handleRequest(HttpServletRequest arg0, HttpServletResponse arg1) throws Exception {
 
-        ModelAndView mav = new ModelAndView("template/index.jsp");
-        mav.addObject("message", "Hello Spring MVC");
+        ModelAndView mav = new ModelAndView("template/hello.jsp");
+        mav.addObject("message", "hello this is a hello page");
         return mav;
     }
 
-    
 }
